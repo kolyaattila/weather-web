3c81e151386d03f5373dcf8be0d5082ee191d5c3
Kolya Attila, INI-DOS-DE-TEO (EXT) <attila.kolya@swisscom.com>
2019-12-06 10:56:25 +0200

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,12 @@ import { AppComponent } from "./app.component";
 
 import { HttpClientModule } from "@angular/common/http";
 import { ForecastItemComponent } from "./components/forecast-item/forecast-item.component";
-import { NumberInputDirective } from "./directives/number-input.directive";
 import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
     ForecastItemComponent,
-    NumberInputDirective
   ],
   imports: [BrowserModule, HttpClientModule, FormsModule],
   providers: [],
